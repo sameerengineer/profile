6601d01f6a85fa1e9845faea948648b245ccb95a
unknown <sameerwebdevloper@gmail.com>
2025-04-28 21:11:53 +0530

    github link included

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import "./Footer.css";
 
@@ -6,7 +7,7 @@ const Footer = () => {
     <footer className="footer">
       <p>© {new Date().getFullYear()} Sameer Shaikh - Front-End Developer</p>
       <ul>
-        <li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+        <li><a href="https://github.com/sameerengineer" target="_blank" rel="noopener noreferrer">GitHub</a></li>
         <li><a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
       </ul>
     </footer>
